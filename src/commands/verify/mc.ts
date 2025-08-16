c1f2c5b0bb99e25b70b0cdeffadcb73cf04069eb
Uncommon-34 <84247691+Uncommon-34@users.noreply.github.com>
2025-08-16 22:27:37 +0100

    Refactor token loading and improve MC verify command
    Moved the loadTokens function from events/ready.ts to utill/functions.ts for better modularity and reuse. Updated ready.ts to use the new loadTokens import and changed the cron job interval from 6 to 12 hours. Enhanced the Minecraft verification command with improved checks for already verified users and added more detailed comments for clarity.

--- src/commands/verify/mc.ts
+++ src/commands/verify/mc.ts
@@ -2,9 +2,12 @@ import { ActionRowBuilder, EmbedBuilder, ModalBuilder, TextInputBuilder, TextInp
 import { createCommandConfig, Flashcore, logger } from 'robo.js'
 import type { ChatInputCommandInteraction } from 'discord.js'
 import type { CommandOptions, CommandResult } from 'robo.js'
-import { connected_players, return_command, tokens } from '../../utill/types'
+import type { connected_players, tokens } from '../../utill/types'
 import { generateCode, getTokens } from '../../utill/functions'
+import { getProfileByDId } from '~/utill/database_functions'
 
+// the command config pretty simple json there are more option avlible check robo.js docs
+// command name is the file name and if in any folders in the command folders are treated as sub commands
 export const config = createCommandConfig({
 	description: 'Unlock the hidden prowess of someone',
 	contexts: ['Guild'],
@@ -17,55 +20,70 @@ export const config = createCommandConfig({
 			required: true
 		}
 	]
-	// sage: {ephemeral: true}
+	// sage: {ephemeral: true}		// commentted out for now maybe enable later
 } as const)
 
+// the main code that executes when the command is used
 export default async (
+	// interaction in the interaction coming from discord for the command
+	// options is the command options we set above
 	interaction: ChatInputCommandInteraction,
 	options: CommandOptions<typeof config>
 ): Promise<CommandResult> => {
+	// declaring variables we need
 	const host = process.env.MC_HOST
 	const h_port = process.env.HUB_PORT
 	const tokens = getTokens() as tokens
 	const username = options['mc-username']
 	const modal = new ModalBuilder().setCustomId('verifiy-mc-code').setTitle('Minecraft Verification Code')
 	const embed = new EmbedBuilder()
-
+	const already_verified = await getProfileByDId(interaction.user.id)
 	let data_hub
 
+	// checking if the user already has verifide
+	if (already_verified !== `error`) return { embeds: [embed.setTitle(`already verified on minecraft`)] }
+
+	// pulls the player list from the Hub server
 	const response_hub = await fetch(`${host}:${h_port}/api/players`, {
 		method: 'GET',
 		headers: {
 			Authorization: `Bearer ${tokens.hub}`
 		}
 	})
+	// checks if the response is ok and if it is sets the player list to data_hub
 	if (!response_hub.ok) {
 		logger.error('Error getting hub players.')
 	} else {
 		data_hub = (await response_hub.json()) as connected_players
 	}
 
+	// checks if data is valid
 	if (!data_hub) {
 		return `hub server is down`
 	}
 
+	//  checks if player is online in the hub server
 	const player = data_hub.online_players.find((p) => p.name === username)
 
-	embed.setColor('Red').setTitle(`No player with username: ${username} connected to Hub`)
+	// if the username isnt found in the player list tells them
+	if (!player)
+		return { embeds: [embed.setColor('Red').setTitle(`No player with username: ${username} connected to Hub`)] }
 
-	if (!player) return { embeds: [embed] }
-
+	// generate's a code to send to the player
 	const code = generateCode()
 
+	// store the username, uuid and code under the users discord id
 	await Flashcore.set(`verify_code-${interaction.user.id}`, code)
 	await Flashcore.set(`verify_code-mc_username-${interaction.user.id}`, player.name)
 	await Flashcore.set(`verify_code-mc_uuid-${interaction.user.id}`, player.uuid)
 
+	// create the message body to send the code to the player
 	const body = {
 		player: `${player.name}`,
 		message: `[MC-UEA VERIFY] Code: ${code}`
 	}
 
+	// send the player a message in the server with the code
 	const response = await fetch(`${host}:${h_port}/api/player/message`, {
 		method: 'post',
 		headers: {
@@ -74,8 +92,10 @@ export default async (
 		body: JSON.stringify(body)
 	})
 
+	// checks the message was sent correctly
 	if (response.status !== 200) {
 		logger.error('Error sending player the code.')
+		return { embeds: [embed.setColor('Red').setTitle(``)] }
 	}
 
 	const one = new TextInputBuilder()
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,8 @@
 import { ActivityType } from 'discord.js'
 import postgres from 'postgres'
-import { client, logger, setState } from 'robo.js'
+import { client, logger } from 'robo.js'
 import { AsyncTask, CronJob, ToadScheduler } from 'toad-scheduler'
-import { token } from '../utill/types'
+import { loadTokens } from '../utill/functions'
 
 export default async () => {
 	logger.ready('Database Connected and ready')
@@ -25,9 +25,9 @@ export default async () => {
 		}
 	})
 
-	const job = new CronJob({ cronExpression: '0 */6 * * *' }, task, { preventOverrun: true })
+	const job = new CronJob({ cronExpression: '0 */12 * * *' }, task, { preventOverrun: true })
 	scheduler.addCronJob(job)
-	logger.ready('started cron job to fetch new tokens every 6th hour')
+	logger.ready('started cron job to fetch new tokens every 12th hour')
 }
 
 export const sql = postgres({
@@ -37,79 +37,3 @@ export const sql = postgres({
 	username: process.env.POSTGRES_USERNAME,
 	password: process.env.POSTGRES_PASSWORD
 })
-
-async function loadTokens() {
-	const user = process.env.MC_USER
-	const pass = process.env.MC_PASS
-	const host = process.env.MC_HOST
-	const h_port = process.env.HUB_PORT
-	const s_port = process.env.SURVIVAL_PORT
-	const c_port = process.env.CREATIVE_PORT
-	const e_port = process.env.EVENT_PORT
-
-	const body = {
-		username: user,
-		password: pass
-	}
-
-	try {
-		const response = await fetch(`${host}:${h_port}/api/auth/login`, {
-			method: 'POST',
-			body: JSON.stringify(body)
-		})
-		if (!response.ok) {
-			logger.error('Error Logging in to hub.')
-		} else {
-			const data = (await response.json()) as token
-			await setState('hub_token', data.token)
-		}
-	} catch (err) {
-		logger.error('Error loading Token for hub:', err)
-	}
-
-	try {
-		const response = await fetch(`${host}:${s_port}/api/auth/login`, {
-			method: 'POST',
-			body: JSON.stringify(body)
-		})
-		if (!response.ok) {
-			logger.error('Error Logging in to survival.')
-		} else {
-			const data = (await response.json()) as token
-			await setState('survival_token', data.token)
-		}
-	} catch (err) {
-		logger.error('Error loading Token for survival:', err)
-	}
-
-	try {
-		const response = await fetch(`${host}:${c_port}/api/auth/login`, {
-			method: 'POST',
-			body: JSON.stringify(body)
-		})
-		if (!response.ok) {
-			logger.error('Error Logging in to creative.')
-		} else {
-			const data = (await response.json()) as token
-			await setState('creative_token', data.token)
-		}
-	} catch (err) {
-		logger.error('Error loading Token for creative:', err)
-	}
-
-	try {
-		const response = await fetch(`${host}:${e_port}/api/auth/login`, {
-			method: 'POST',
-			body: JSON.stringify(body)
-		})
-		if (!response.ok) {
-			logger.error('Error Logging in to event.')
-		} else {
-			const data = (await response.json()) as token
-			await setState('event_token', data.token)
-		}
-	} catch (err) {
-		logger.error('Error loading Token for event:', err)
-	}
-	logger.info(`logged in to all servers and stored tokens`)
-}
