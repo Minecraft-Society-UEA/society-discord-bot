2e9bd69e7455c9f7a5502114d6854bca38857fcb
Uncommon-34 <84247691+Uncommon-34@users.noreply.github.com>
2025-09-18 01:23:19 +0100

    Rename set-roles command to role-storage
    Renamed the admin command file from set-roles.ts to role-storage.ts and removed unused imports to improve clarity and maintainability.

--- src/commands/admin/role-storage.ts
+++ src/commands/admin/role-storage.ts
@@ -1,10 +1,8 @@
-import { EmbedBuilder, flatten, PermissionFlagsBits } from 'discord.js'
+import { PermissionFlagsBits } from 'discord.js'
 import { createCommandConfig, Flashcore } from 'robo.js'
-import type { ChatInputCommandInteraction, GuildMember, Role } from 'discord.js'
+import type { ChatInputCommandInteraction, Role } from 'discord.js'
 import type { CommandOptions, CommandResult } from 'robo.js'
-import { getProfileByDId, updatePlayerProfile } from '../../utill/database_functions'
-import { db_player, role_storage } from '../../utill/types'
-import { mc_command, message_player, online_server_check } from '../../utill/functions'
+import { role_storage } from '../../utill/types'
 
 // the command config pretty simple json there are more option avlible check robo.js docs
 // command name is the file name and if in any folders in the command folders are treated as sub commands
