602b54bc9f840059ee0df8a39dfd62f891e0ff50
Cai <116274434+AxonViper@users.noreply.github.com>
2026-03-16 12:43:57 +0000

    Replicating the exact <div> structure Gmail uses

--- src/utill/functions/email/code.ts
+++ src/utill/functions/email/code.ts
@@ -2,39 +2,18 @@ import { transporter } from '~/utill'
 
 export async function emailCode(email: string, code: string, username: string) {
 	const VERIFICATION_CODE = code
-	const SERVER_NAME = `UEA Minecraft Society`
 	const NAME = username
 	const SUPPORT_EMAIL = process.env.GMAIL_USER as string
 
+	const plainText = `UEA Minecraft Society\nHey ${NAME},\n${VERIFICATION_CODE}\n\nHope to see you on the server soon!\n\nThe MCSOC Team`
+
+	const gmailHtml = `<div dir="ltr"><div style="color:rgb(196,196,196);font-family:sans-serif;display:flex"><div style="font-size:14px;font-weight:600;letter-spacing:-0.5px"><br class="gmail-Apple-interchange-newline">UEA Minecraft Society</div></div><h3 style="color:rgb(23,22,21);font-family:sans-serif;margin:30px 0px 10px;font-size:1.5rem;font-weight:500">Hey ${NAME},</h3><div style="color:rgb(200,195,188);font-family:sans-serif;text-align:center"><div style="background:rgb(241,241,241);color:rgb(23,22,21);display:inline-block;border-radius:12px;padding:15px 20px;margin:10px;font-size:40px;letter-spacing:2px">${VERIFICATION_CODE}</div><div class="gmail-adL" style="scrollbar-color: rgb(69, 74, 77) rgb(32, 35, 36);"></div></div><div class="gmail-adL" style="color:rgb(200,195,188);font-family:sans-serif"><div class="gmail-adm" style="margin:5px 0px"></div><div class="gmail-im" style="color:inherit"><p style="margin:0px 0px 10px;font-size:14px">Hope to see you on the server soon!</p><p style="color:rgb(152,152,152);font-size:12px;margin:0px">The MCSOC Team</p></div></div></div>`
+
 	await transporter.sendMail({
-		from: `"${SERVER_NAME}" <${SUPPORT_EMAIL}>`,
+		from: `"Minecraft Society UEA" <${SUPPORT_EMAIL}>`,
 		to: `${email}`,
 		subject: 'Your verification code for the UEA Minecraft server',
-		text: `UEA Minecraft Society\n\nHey ${NAME},\n\n${VERIFICATION_CODE}\n\nHope to see you on the server soon!\n\nThe MCSOC Team`,
-		html: `
-<html lang="en">
-<head>
-</head>
-<body style="margin:0; padding:20px 20px 20px 20px; font-family:sans-serif; color:#333;">
-	<div style="max-width:450px; margin:50px auto; padding:20px 35px 20px 35px; background:#fff; border:1px solid #dddddd; border-radius:12px; box-shadow:0 2px 20px rgba(0,0,0,0.05); position:relative;">
-		<h3 style="margin:30px 0 10px; font-size:1.5rem; font-weight:500; color:#171615;">
-			Hey ${NAME},
-		</h3>
-		<div style="text-align:center;">
-			<div style="display:inline-block; text-align:center; background:#f1f1f1; border-radius:12px; padding:15px 20px; margin:10px; font-size:40px; font-weight:400; color:#171615; letter-spacing:2px; font-family:sans-serif;">
-				${VERIFICATION_CODE}
-			</div>
-		</div>
-		<p style="margin:0 0 10px 0; font-size:14px; font-weight:500;">
-			Hope to see you on the server soon!
-		</p>
-		<p style="font-size:12px; color:#989898; margin:0;">
-			The MCSOC Team
-		</p>
-		<p></p>
-	</div>
-</body>
-</html>
-`
+		text: plainText,
+		html: gmailHtml
 	})
 }
