b762c9cfc5406adcd70ff1669b2b0e8c0aea0c51
Uncommon <84247691+Uncommon-34@users.noreply.github.com>
2026-06-30 21:07:20 +0100

    gg

--- src/utill/constants/servers.ts
+++ src/utill/constants/servers.ts
@@ -1,4 +1,4 @@
 // server key type
-export type ServerKey = 'The Hub' | 'SMP 1' | 'SMP 2' | 'Creative' | 'OLD SMPs' | 'Event HG'
+export type ServerKey = 'The Hub' | 'SMP' | 'Creative' | 'OLD SMPs' | 'Factions' | 'Event HG' | 'Event Spleef'
 
-export const HUB_SERVER_ID = 'a406fbb6-418d-4160-8611-1c180d33da14'
+export const HUB_SERVER_ID = '31b0d944'
--- src/utill/constants/servers_options.ts
+++ src/utill/constants/servers_options.ts
@@ -36,23 +36,19 @@ export const servers_non_req = {
 	choices: [
 		{
 			name: `Hub`,
-			value: `a406fbb6-418d-4160-8611-1c180d33da14`
+			value: `31b0d944`
 		},
 		{
 			name: `SMP`,
-			value: `ca3cf618-b941-478c-b056-fedd9ba9f1f0`
+			value: `a814b3bd`
 		},
 		{
-			name: `Creative`,
-			value: `8dbb710c-c181-4cb4-a236-f2dec3bd08e6`
+			name: `Factions`,
+			value: `bd04a936`
 		},
 		{
-			name: `OLD SMPs`,
-			value: `094a4a23-d454-4d3e-9c14-7432e12089cb`
-		},
-		{
-			name: `Event HG`,
-			value: `6d75e064-dec0-4213-951a-bf265be15854`
+			name: `Event Spleef`,
+			value: `c77745c3`
 		}
 	],
 	required: true
